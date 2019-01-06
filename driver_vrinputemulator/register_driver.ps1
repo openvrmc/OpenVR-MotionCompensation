4f63298d198c731cf215bcaae44b06830fcefdb4
Lavovski <igormitric@gmail.com>
2019-01-06 17:28:35 +0100

    make driver directly loadable (no installation required)

---
+++ driver_vrinputemulator/register_driver.ps1
@@ -0,0 +1,2 @@
+& "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrpathreg.exe" adddriver $PWD
+& "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrpathreg.exe"
---
+++ driver_vrinputemulator/register_driver_undo.ps1
@@ -0,0 +1,2 @@
+& "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrpathreg.exe" removedriver $PWD
+& "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrpathreg.exe"
