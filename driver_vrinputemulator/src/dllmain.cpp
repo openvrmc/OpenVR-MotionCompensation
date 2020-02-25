27b016c999e66e44a39a0d0c73d03a41db9c40e3
Dschadu <darki.klein@gmail.com>
2020-02-25 19:00:24 +0100

    Setup Solution

--- driver_vrinputemulator/src/dllmain.cpp
+++ driver_vrinputemulator/src/dllmain.cpp
@@ -11,35 +11,41 @@ const char* logConfigDefault =
 "	TO_STANDARD_OUTPUT = true\n"
 "	MAX_LOG_FILE_SIZE = 2097152 ## 2MB\n"
 "* TRACE:\n"
-"	ENABLED = false\n"
+"	ENABLED = true\n"
 "* DEBUG:\n"
-"	ENABLED = false\n";
+"	ENABLED = true\n";
 
 INITIALIZE_EASYLOGGINGPP
 
-void init_logging() {
+void init_logging()
+{
 	el::Loggers::addFlag(el::LoggingFlag::DisableApplicationAbortOnFatalLog);
 	el::Configurations conf(logConfigFileName);
 	conf.parseFromText(logConfigDefault);
-	conf.parseFromFile(logConfigFileName);
+	//conf.parseFromFile(logConfigFileName);
 	conf.setRemainingToDefault();
 	el::Loggers::reconfigureAllLoggers(conf);
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 ) {
-	switch (ul_reason_for_call) {
-		case DLL_PROCESS_ATTACH:
-			init_logging();
-			LOG(INFO) << "VRInputEmulator dll loaded...";
-			break;
-		case DLL_THREAD_ATTACH:
-		case DLL_THREAD_DETACH:
-		case DLL_PROCESS_DETACH:
-			break;
+BOOL APIENTRY DllMain(HMODULE hModule,
+					  DWORD  ul_reason_for_call,
+					  LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBoxA(NULL, "Loaded!", "Motion Compensation", MB_OK);
+		init_logging();
+		LOG(INFO) << "|========================================================================================|";
+		LOG(INFO) << "VRInputEmulator dll loaded...";
+		LOG(TRACE) << "Trace messages enabled.";
+		LOG(DEBUG) << "Debug messages enabled.";
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
 	}
 	return TRUE;
 }
-
