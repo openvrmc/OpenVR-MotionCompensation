c8ce2bd6fc1d725020abf5c40f9a5e5a2d2c89a1
Thomas van Gemert <tvg@di.ku.dk>
2023-02-14 21:09:19 +1100

    Cleaned up driverlib VCXPROJ and added new easylogging fix.

--- driver_vrmotioncompensation/src/logging.h
+++ driver_vrmotioncompensation/src/logging.h
@@ -3,12 +3,12 @@
 // easylogging includes
 #ifdef NDEBUG
 #undef NDEBUG
-#define ELPP_THREAD_SAFE
-#define ELPP_NO_DEFAULT_LOG_FILE
+//#define ELPP_THREAD_SAFE
+//#define ELPP_NO_DEFAULT_LOG_FILE
 #include <easylogging++.h>
 #define NDEBUG
 #else
-#define ELPP_THREAD_SAFE
-#define ELPP_NO_DEFAULT_LOG_FILE
+//#define ELPP_THREAD_SAFE
+//#define ELPP_NO_DEFAULT_LOG_FILE
 #include <easylogging++.h>
 #endif
