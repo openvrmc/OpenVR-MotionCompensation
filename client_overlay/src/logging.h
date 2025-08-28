5b9e2dc21f17580b48c3ca94ae4637e13d9e0035
Wagnard <wagnard28@gmail.com>
2025-08-28 15:54:37 -0400

    - Updated boost to 1_89 for the client_overlay. - Updated QT version to QT 5.15.2 for the client_overlay. - Moved back the Platform toolset of all project to v142 for simplicity. (Mostly due to QT)

--- client_overlay/src/logging.h
+++ client_overlay/src/logging.h
@@ -3,14 +3,14 @@
 // easylogging includes
 #ifndef _DEBUG
 	#define _DEBUG
-	#define ELPP_THREAD_SAFE
-	#define ELPP_QT_LOGGING
-	#define ELPP_NO_DEFAULT_LOG_FILE
+	//#define ELPP_THREAD_SAFE
+	//#define ELPP_QT_LOGGING
+	//#define ELPP_NO_DEFAULT_LOG_FILE
 	#include <easylogging++.h>
 	#undef _DEBUG
 #else
-	#define ELPP_THREAD_SAFE
-	#define ELPP_QT_LOGGING
-	#define ELPP_NO_DEFAULT_LOG_FILE
+	//#define ELPP_THREAD_SAFE
+	//#define ELPP_QT_LOGGING
+	//#define ELPP_NO_DEFAULT_LOG_FILE
 	#include <easylogging++.h>
 #endif
